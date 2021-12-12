255ed7a4f4cf92ec586facf22946cd67a2fdbf9d
Sherif Magdy <Sherif_shanner@hotmail.com>
2021-12-12 20:35:39 +0800

    updated client home Page. Created Admin Login admin home page redirect after successfull login

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ParcelTrackingSystem.Controllers
 {
     public class AdminController : Controller
     {
+        [Authorize(Roles = "Admin")]
         public IActionResult Index()
         {
             return View();
